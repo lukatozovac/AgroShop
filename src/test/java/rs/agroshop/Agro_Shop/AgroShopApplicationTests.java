2d0b887485b6420a8718fb8e1b5503cc64e39125
lukatozovac <lukatodorovicnovapazova@gmail.com>
2026-05-29 15:23:48 +0200

    Initial commit

---
+++ src/test/java/rs/agroshop/Agro_Shop/AgroShopApplicationTests.java
@@ -0,0 +1,13 @@
+package rs.agroshop.Agro_Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
